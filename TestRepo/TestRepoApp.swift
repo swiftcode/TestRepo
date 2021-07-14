c4df2e5bbfdbbaacb22d75dc63a51b781f76bf50
swiftcode <mpc@junct.com>
2021-07-14 11:33:17 -0500

    Initial Commit

---
+++ TestRepo/TestRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestRepoApp.swift
+//  TestRepo
+//
+//  Created by mpc on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct TestRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
